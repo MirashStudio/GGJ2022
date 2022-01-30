8cefb169af4b4a652430c0a8dba14cc6207d6e53
gilmarkandrade <45281863+gilmarkandrade@users.noreply.github.com>
2022-01-30 13:13:44 -0300

    up

--- Game/scenes/background.gd
+++ Game/scenes/background.gd
@@ -12,19 +12,7 @@ var completed_appear_space = false
 func _ready():
 	
 	GameSingleton.connect("levelUP",self,"level_UP")
-	yield(get_tree().create_timer(2),"timeout")
-	space = get_node("space")
-	cosmos = get_node("Cosmos")
-	stars = get_node("ParallaxBackground/ParallaxStars")
-	planets = get_node("ParallaxBackground/planets")
-	via_lactea = get_node("ParallaxBackground/ParallaxVialactea")
-	print(space)
-	print(via_lactea)
-	print(cosmos)
-	print(planets)
-	print(stars)
-	set_visibiliy_space()
-	set_visibiliy_viaLactea()
+
 	
 func level_UP():
 	
@@ -34,16 +22,15 @@ func level_UP():
 		2: 
 			set_visibiliy_viaLactea()
 		3:
-			planets.visible = true
-			stars.visible = true
-		4:
-			
+			$"../animlevel".play("stars")
+		4:	
 			set_visibiliy_galaxy()
 
 
 func set_visibiliy_viaLactea():
 	if completed_appear_via_lactea == false:
 		completed_appear_via_lactea = true
+		#$"../animlevel".play("vialactea")
 		var mod = 0.0
 		via_lactea.modulate = Color(1,1,1,0)
 		cosmos.visible = true
@@ -58,13 +45,14 @@ func set_visibiliy_viaLactea():
 func set_visibiliy_galaxy():
 	if completed_appear_galaxy == false:
 		completed_appear_galaxy = true
-		"""galaxy.visible = true
+		#$"../animlevel".play("galaxy")
+		galaxy.visible = true
 		var mod = 0.0
 		galaxy.modulate = Color(1,1,1,0)
 		while mod < 1:
 			yield(get_tree().create_timer(0.02),"timeout")
 			mod += 0.01 
-			galaxy.modulate = Color(1,1,1,mod)"""
+			galaxy.modulate = Color(1,1,1,mod)
 			
 		galaxy.modulate = Color(1,1,1,1)
 
@@ -75,8 +63,8 @@ func set_visibiliy_space():
 	
 	if completed_appear_space == false:
 		completed_appear_space = true
-		$"../animlevel".play("space")
-		"""space.visible = true
+		#$"../animlevel".play("space")
+		space.visible = true
 		var mod = 0.0
 		space.modulate = Color(1,1,1,0)
 		while mod < 1:
@@ -84,4 +72,4 @@ func set_visibiliy_space():
 			mod += 0.01 
 			space.modulate = Color(1,1,1,mod)
 			
-		space.modulate = Color(1,1,1,1)"""
+		space.modulate = Color(1,1,1,1)
