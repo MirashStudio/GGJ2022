bf1997fdd96f2e4bf6cbeb1c0abd8f239e997aec
gilmarkandrade <45281863+gilmarkandrade@users.noreply.github.com>
2022-01-30 15:12:02 -0300

    upt

--- Game/scenes/background.gd
+++ Game/scenes/background.gd
@@ -1,16 +1,17 @@
 extends CanvasLayer
+onready var espace = get_node("space")
+onready var cosmos = get_node("Cosmos")
+onready var stars = get_node("ParallaxBackground/ParallaxStars")
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func level_UP():
+	
+	match GameSingleton.level_player:
+		1:
+			pass
+			
+		2: 
+			pass
+		3:
+			
+			pass
